d219c897371ffdee33d7334969688c4e887a8dad
kushagra mangalam <kushagra.mangalam@lpu.in>
2025-06-16 13:51:58 +0530

    first

---
+++ java_script_start/01.js
@@ -0,0 +1,17 @@
+// let people={
+//     name: 'kushagra mangalam',
+//     age: 21
+// };
+// console.log(people.name);
+// console.log(people);
+// window.console.log("hello from js");
+// window.console.log("age:",age1);
+// // var age1=21;    //declaration and initialization
+// let age1=29; //re-declaration and re-initialization
+// window.console.log("age:",age1);
+// let x=10+12;
+// console.log(x);             
+// console.log("age: " ,age); 
+let age=22;
+console.log("age: " ,age); 
+console.log('a'*20);
